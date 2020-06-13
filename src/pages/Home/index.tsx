9fde12cf8f898a5e6161ac3f20c8fadf3a69dce4
Nicolas Lopes Aquino <nicolaslopesbusiness@gmail.com>
2020-06-13 11:58:11 -0300

    Index Templates By API

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Wrapper, Card, Templates, Form, Button } from "./styles";
 import logo from "../../assets/logo.svg";
+import { api } from "../../services/api";
+
 export default function Home() {
+  interface TemplateInterface {
+    id: number;
+    url: string;
+    name: string;
+    count: number;
+  }
+  const [templates, setTemplates] = useState<TemplateInterface[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get("/get_memes");
+      const {
+        data: { memes },
+      } = response.data;
+
+      setTemplates(memes);
+    })();
+  }, []);
   return (
     <Wrapper>
       <img src={logo} alt="logo" />
       <Card>
         <h2>Select a template</h2>
         <Templates>
-          <button type="button">
-            <img src="" alt="Template"></img>
-          </button>
+          {templates.map((template) => (
+            <button key={template.id} type="button">
+              <img src={template.url} alt={template.name} />
+            </button>
+          ))}
         </Templates>
         <h2>Texts</h2>
         <Form>
